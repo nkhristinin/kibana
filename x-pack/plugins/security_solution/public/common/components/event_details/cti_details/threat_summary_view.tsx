07244cbf9c1bece978c00d5f9f67547fb430b657
Khristinin Nikita <nikita.khristinin@elastic.co>
2022-09-13 19:10:52 +0200

    Fix types

--- x-pack/plugins/security_solution/public/common/components/event_details/cti_details/threat_summary_view.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/cti_details/threat_summary_view.tsx
@@ -24,6 +24,7 @@ import type { CtiEnrichment } from '../../../../../common/search_strategy/securi
 import type {
   BrowserFields,
   TimelineEventsDetailsItem,
+  RiskSeverity,
 } from '../../../../../common/search_strategy';
 import { HostRiskSummary } from './host_risk_summary';
 import { EnrichmentSummary } from './enrichment_summary';
@@ -144,7 +145,7 @@ const ThreatSummaryViewComponent: React.FC<{
 
   const originalHostRisk = data?.find(
     (eventDetail) => eventDetail?.field === 'host.risk.calculated_level'
-  )?.values?.[0];
+  )?.values?.[0] as RiskSeverity | undefined;
 
   return (
     <>
